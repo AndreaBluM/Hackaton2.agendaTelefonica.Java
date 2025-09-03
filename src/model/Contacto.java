8003cb19b64ffbf9db3594b1febb44a3ee86972a
andre <lauralme96@gmail.com>
2025-09-03 11:04:02 -0500

    persona y contacto clases creadas

--- src/model/Contacto.java
+++ src/model/Contacto.java
@@ -1,4 +1,42 @@
 package model;
 
-public class Contacto extends Persona{
+public class Contacto extends Persona {
+    private String telefono;
+    private String email;
+
+    public Contacto() {
+    }
+
+    public Contacto(String nombre, String apellido, String telefono, String email) {
+        super(nombre, apellido);
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        if (!telefono.matches("\\d{9}"))
+            throw new IllegalArgumentException("El teléfono debe tener 9 dígitos.");
+        this.telefono = telefono;
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        if (!email.contains("@") || !email.contains("."))
+            throw new IllegalArgumentException("El email debe contener '@' y '.'");
+        this.email = email;
+    }
+
+
+    @Override
+    public String toString() {
+        return nombre + " " + apellido + " - " + telefono;
+    }
 }
--- src/model/Persona.java
+++ src/model/Persona.java
@@ -1,4 +1,53 @@
 package model;
 
 public abstract class Persona {
+    protected String nombre;
+    protected String apellido;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String apellido) {
+        if (nombre == null || nombre.isEmpty() || apellido == null || apellido.isEmpty()){
+            throw new IllegalArgumentException("nombre y apellido no pueden estar vacíos..");
+        }
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public int hashCode() {
+        return (nombre.toLowerCase() + apellido.toLowerCase()).hashCode();
+    }
+
+    // metodo si los nombres son iguales en contenido
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Persona)) return false;
+
+        Persona p = (Persona) obj;
+        return nombre.equalsIgnoreCase(p.nombre) &&
+                apellido.equalsIgnoreCase(p.apellido);
+    }
+    public String mostrarInfo(){
+        return nombre + " " + apellido;
+    }
 }
+
