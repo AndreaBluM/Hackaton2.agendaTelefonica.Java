c443f05bca9cc74f345387042b9acd3c1e0f53cb
andre <lauralme96@gmail.com>
2025-09-03 10:41:15 -0500

    creacion clase contacto

--- src/Contacto.java
+++ src/Contacto.java
@@ -1,2 +1,11 @@
-public class Contacto {
+public class Contacto extends Persona {
+    private String telefono;
+    private String email;
+
+
+}
+
+@Override
+public String toString(){
+    return nombre + " " + apellido + " - " + email + " - " + telefono;
 }
